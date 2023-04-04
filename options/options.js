ce9da3d2d11cbb77db285b693581408482e7bd7e
Crowdbotics <team@crowdbotics.com>
2023-04-04 18:06:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-wildflower-39708",
+  url: "https://holy-wildflower-39708.botics.co",
+  api: "https://holy-wildflower-39708.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
